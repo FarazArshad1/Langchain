8199a9592e78f48985ac9e46ce3492a7ab453d60
Faraz Arshad <faraz.arshad125@gmail.com>
2025-10-21 19:41:29 +0500

    structured output with typedict

--- Structured Output/2.with_structured_output_type_dict.py
+++ Structured Output/2.with_structured_output_type_dict.py
@@ -1,2 +1,27 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+from typing import TypedDict, Annotated, Optional, Literal
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+
+# Review
+class Review(TypedDict):
+    summary: Annotated[str, "A brief funny summary of the review"]
+    sentiment: Annotated[Literal["pos","neg"], "Retuen sentiment of the review either neagtive, positive or neutral"]
+    key_themes: Annotated[list[str],"Write down all the key themes discussed in the review"]
+    pros: Annotated[Optional[list[str]],"Write down all the pros inside a list"]
+    cons: Annotated[Optional[list[str]],"Write down all the cons inside a list"]
+
+structured_output = model.with_structured_output(Review)
+
+result = structured_output.invoke(
+    """ The hardware is great, but the software feels bloated.
+                                  There are too many pre-installed apps taht I can't remove. ALso, the UI looks outdated
+                                  compared to other brands, Hoping for a software update to fix this"""
+)
+
+
+print(result)
