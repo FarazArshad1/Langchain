27306f5b9fe1e6d79c67c5345ec1cf2c85bbc12c
FarazArshad1 <faraz.arshad125@gmail.com>
2025-10-30 08:33:12 +0500

    Recursive Character Text Splitter

--- Text Splitters/1.length_based_splitter.py
+++ Text Splitters/1.length_based_splitter.py
@@ -1,10 +1,15 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+"""
+Read more about text splitter here
+https://docs.langchain.com/oss/python/integrations/splitters/index
+"""
+
+from langchain_text_splitters import CharacterTextSplitter
 
 with open('Document Loader/Documents/cricket.txt') as f:
     document = f.read()
 
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap = 0)
+text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap = 0)
 texts = text_splitter.split_text(document)
 
 print(texts)
---
+++ Text Splitters/2.text_structure_based_splitter.py
@@ -0,0 +1,10 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+with open("Document Loader/Documents/cricket.txt") as f:
+    document = f.read()
+
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+texts = text_splitter.split_text(document)
+
+print(texts)
